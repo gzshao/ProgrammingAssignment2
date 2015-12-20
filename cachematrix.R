5bbc844de5e02f87f6040cc1385754a45dc2fbaa
petersgz@gmail.com <Guozheng@Guozhengs-MacBook-Pro.local>
2015-12-19 21:06:42 -0800

    my completed code to store to cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## This R file is used to store and get a matrix and its inverse
+## matrix into/from cache. To use it, you need to first run the
+## first function makeCacheMatrix(), and assign it to a matrix, say
+## tmp<-makeCacheMatrix(matrix(1:4),2,2).
 
-makeCacheMatrix <- function(x = matrix()) {
+## Now you have the tmp matrix, you can get its inverse matrix by
+## calling the cacheSolve() function.
 
+
+## This first function makeCacheMatrix is a list of four functions
+## to set, get the original matrix and its inverse matrix, it does
+## the work of read and write only. It doesn't do any matrix calculation.
+makeCacheMatrix<-function(x=matrix()){
+    inv<-NULL
+    set<-function(y){
+        x<<-y
+        inv<<-NULL
+    }
+    
+    get<-function() x
+    
+    setInverse<-function(inverse){
+        inv<<-inverse
+    }
+    
+    getInverse<-function() inv
+    
+    list(set = set, get = get,
+    setInverse = setInverse,
+    getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## This second function reads the inverse matrix. In case it doesn't
+## already exist, it will calculate the inverse matrix, store it
+## into cache and return it.
+cacheSolve<-function(x=matrix(),...){
+    inv<-x$getInverse()
+    if(!is.null(inv)){
+        message("getting cache data")
+        return(m)
+    }
+    
+    data<-x$get()
+    inv<-solve(data,...)
+    x$setInverse(inv)
+    inv
